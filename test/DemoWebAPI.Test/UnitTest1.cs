30bc4d2ed5e48f71dce9f16cc0a9d7951c996272
Ck. Nitin <ck.nitin@outlook.com>
2020-06-07 13:20:27 +0530

    Initial Commit

---
+++ test/DemoWebAPI.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using DemoWebAPI.Controllers;
+
+namespace DemoWebAPI.Test
+{
+    public class UnitTest1
+    {
+
+        WeatherForecastController controller = new WeatherForecastController();
+        [Fact]
+        public void Test1()
+        {
+            var returnValue = controller.GetWeatherName();
+            Assert.Equal("spring", returnValue.ToString());
+        }
+    }
+}
